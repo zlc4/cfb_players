a5452a45a7d470185fe6a686da597bb34d3c891e
Zach Cartledge <78279322+zlc4@users.noreply.github.com>
2026-05-25 21:59:15 -0400

    Update load_players.sql

--- load_players.sql
+++ load_players.sql
@@ -18,23 +18,24 @@ BEGIN TRY
 	SELECT * FROM player_staging;
 
 	-- Merge the data into the teams table
-	MERGE players as target USING player_staging as source ON target.player_id = source.player_id
+	MERGE player as target USING player_staging as source ON target.player_id = source.player_id
 		WHEN MATCHED THEN
 			UPDATE SET
 			target.jersey_number = source.jersey_number,
-			target.player_name = source.player_name,
+			target.player_fname = source.player_fname,
+			target.player_lname = source.player_lname,
 			target.team_id = source.team_id,
 			target.position = source.position,
 			target.height = source.height,
 			target.player_weight = source.player_weight
 
 		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (jersey_number, player_name, team_id, position, height, player_weight)
-				VALUES(source.jersey_number, source.player_name, source.team_id, source.position,
+			INSERT (player_id, jersey_number, player_fname, player_lname, team_id, position, height, player_weight)
+				VALUES(source.player_id, source.jersey_number, source.player_fname, source.player_lname, source.team_id, source.position,
 						source.height, source.player_weight)
 
-		WHEN NOT MATCHED BY SOURCE THEN
-			DELETE
+		--WHEN NOT MATCHED BY SOURCE THEN
+			--DELETE
 
 		OUTPUT $action as action_taken, inserted.*, deleted.*;
 	
